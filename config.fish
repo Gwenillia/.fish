fc9ff23f8bd8003e1e18f00c8ca06ce4e5563c72
Gwenillia <gwenden31@gmail.com>
2023-09-22 23:40:05 +0200

    remove os specific

--- config.fish
+++ config.fish
@@ -52,15 +52,6 @@ function __check_rvm --on-variable PWD --description 'Do nvm stuff'
   end
 end
 
-switch (uname)
-  case Darwin
-    source (dirname (status --current-filename))/config-osx.fish
-  case Linux
-    source (dirname (status --current-filename))/config-linux.fish
-  case '*'
-    source (dirname (status --current-filename))/config-windows.fish
-end
-
 set LOCAL_CONFIG (dirname (status --current-filename))/config-local.fish
 if test -f $LOCAL_CONFIG
   source $LOCAL_CONFIG
