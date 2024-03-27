5ff06ab271b8aefb70964a04baeb31da21579390
Gwen Tripet-Costet <gwenden31@gmail.com>
2024-03-27 11:31:56 +0100

    Add gitRtm command

--- config.fish
+++ config.fish
@@ -22,7 +22,9 @@ if type -q exa
   alias lla "ll -a"
 end
 
+# export mac stuff
 export STARSHIP_DISTRO=""
+export MUSIC_APP="Music"
 
 # set global  variables
 set -gx EDITOR vim
@@ -52,6 +54,34 @@ function __check_rvm --on-variable PWD --description 'Do nvm stuff'
     nvm use
   else
   end
+end
+
+function gitRtm -d "Will merge specific branch to master"
+  set branch $argv[1]
+  set target_branch $argv[2]
+  
+  if test -z "$branch"
+    echo "Usage: gitRtm <branch> [target_branch]"
+    return 1
+  end
+
+  if test -z "$target_branch"
+    set target_branch "master"
+  end
+
+  echo "Checking out $branch..."
+  g checkout $branch
+  g pull
+
+  echo "Checking out $target_branch..."
+  g checkout $target_branch
+  g pull
+
+  echo "Merging $branch into $target_branch..."
+  g merge $branch
+  g push
+
+  echo "Done."
 end
 
 set LOCAL_CONFIG (dirname (status --current-filename))/config-local.fish
