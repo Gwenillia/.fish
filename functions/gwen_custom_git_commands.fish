21fcf23cecc7742547e25ad10a8ef9fb35b872fb
Gwenillia <55385809+Gwenillia@users.noreply.github.com>
2025-01-09 13:21:27 +0100

    text title pr (#1)
    * gpcr
    
    * text commit
    
    * text commit 2
    
    * text commit 3
    
    * text commit 4
    
    * text commit 5
    
    * text commit 6
    
    * text commit 6
    
    * text commit 8
    
    * text commit 9
    
    * text commit 10
    
    * text commit 11
    
    * text commit 12

--- functions/gwen_custom_git_commands.fish
+++ functions/gwen_custom_git_commands.fish
@@ -1,12 +1,12 @@
 function gwen_custom_git_commands
   if test (count $argv) -lt 1
-    echo "Usage: custom_git_commands [help|rtm|nah|flist|forget|uncommit]"
+    echo "Usage: custom_git_commands [help|rtm|nah|flist|forget|uncommit|gpcr]"
     return
   end
 
   switch $argv[1]
     case "help"
-      echo "Usage: custom_git_commands [help|rtm|nah|flist|forget|uncommit]"
+      echo "Usage: custom_git_commands [help|rtm|nah|flist|forget|uncommit|gpcr]"
       echo
       echo "Subcommands:"
       echo "  rtm        Merges specific branch to master"
@@ -14,6 +14,21 @@ function gwen_custom_git_commands
       echo "  flist      Fetches, checks for gone branches, deletes them or says none"
       echo "  forget     Lists branches that are gone"
       echo "  uncommit   Backs out of the last commit but keeps the changes"
+      echo "  gpcr       Create a PR on github"
+      echo
+      echo "gpcr Usage:"
+      echo "  gpcr [target_branch] \"PR title\""
+      echo
+      echo "Examples:"
+      echo "  gpcr feat/xxx-x-x \"Fix blabla\""
+      echo "  gpcr \"Awesome pr Title\""
+      echo
+      echo "rtm Usage:"
+      echo "  rtm branch_name [target_branch]"
+      echo
+      echo "Examples:"
+      echo "  rtm staging"
+      echo "  rtm staging nightly"
     case "rtm"
       _gwen_custom_git_commands_rtm $argv[2..-1]
     case "nah"
@@ -24,6 +39,8 @@ function gwen_custom_git_commands
       _gwen_custom_git_commands_forget
     case "uncommit"
       _gwen_custom_git_commands_uncommit
+    case "gpcr"
+      _gwen_custom_git_commands_gpcr $argv[2..-1]
     case "*"
       echo "Unknown subcommand: $argv[1]"
   end
